06f85759b45fffc253bbfd32a30e92cd51729c94
Pedro Alberto <pedroalbertompontes@gmail.com>
2024-06-01 13:54:51 -0300

    Added the background to the start page

--- src/screens/StartScreen.js
+++ src/screens/StartScreen.js
@@ -1,37 +1,48 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, ImageBackground } from 'react-native';
+
+const backgroundImage = require('../../assets/images/background.png');
 
 const StartScreen = ({ navigation }) => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Bem-vindo ao Jogo do Peixe</Text>
-      <TouchableOpacity style={styles.startButton} onPress={() => navigation.navigate('Game')}>
-        <Text style={styles.startButtonText}>Iniciar Jogo</Text>
-      </TouchableOpacity>
-    </View>
-  );
+    return (
+        <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
+            <View style={styles.container}>
+                <Text style={styles.title}>Bem-vindo ao Jogo do Peixe</Text>
+                <TouchableOpacity style={styles.startButton} onPress={() => navigation.navigate('Game')}>
+                    <Text style={styles.startButtonText}>Iniciar Jogo</Text>
+                </TouchableOpacity>
+            </View>
+        </ImageBackground>
+    );
 };
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'cyan',
-  },
-  title: {
-    fontSize: 32,
-    marginBottom: 20,
-  },
-  startButton: {
-    padding: 10,
-    backgroundColor: 'blue',
-    borderRadius: 5,
-  },
-  startButtonText: {
-    color: 'white',
-    fontSize: 20,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 32,
+        marginBottom: 20,
+        color: 'white', // Altere a cor do título conforme necessário
+    },
+    startButton: {
+        padding: 10,
+        backgroundColor: 'blue',
+        borderRadius: 5,
+    },
+    startButtonText: {
+        color: 'white',
+        fontSize: 20,
+    },
 });
+
 
 export default StartScreen;
